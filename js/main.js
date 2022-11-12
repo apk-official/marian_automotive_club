5379c253ce3da88205be375927fe2b1db2cbe65d
Abhinav P Kumar <abhinavpkumar@Abhinavs-MacBook-Air.local>
2022-11-12 09:34:18 +0530

    testimonial autoplay added 🏎

--- js/main.js
+++ js/main.js
@@ -33,10 +33,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   grabCursor: true,
   loop: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
